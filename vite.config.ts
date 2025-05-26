a542b5d1e7d2f1ea659b83d6b1f6a265b0014b09
ls-tb <Lxs249638467@gmail.com>
2025-05-26 17:02:57 +0800

    实现单文件打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue(), viteSingleFile()],
+	// base: "./",
+	// build: {
+	// 	assetsInlineLimit: 100000000, // 所有资源内
+	// 	rollupOptions: {
+	// 		output: {
+	// 			manualChunks: undefined,
+	// 		},
+	// 	},
+	// },
+});
